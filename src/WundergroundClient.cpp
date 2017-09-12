4afa27657ae96ceff8aa297ffb73eac57a2612ae
Jon-Young-Us <jonyoung.us@gmail.com>
2017-09-11 22:06:08 -0600

    More tabs replaced with whitespace

--- src/WundergroundClient.cpp
+++ src/WundergroundClient.cpp
@@ -325,8 +325,8 @@ void WundergroundClient::value(String value) {
 
    if (currentKey == "local_time_rfc822") {
     date = value.substring(0, 16);
-	localTime = value.substring(17, 24);
-	utcOffset = value.substring(26);
+    localTime = value.substring(17, 24);
+    utcOffset = value.substring(26);
   }
 
   if (currentKey == "observation_time_rfc822") {
