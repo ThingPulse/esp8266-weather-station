68c99f6802758903eb89d971290ca5c2a6816ebd
Daniel Eichhorn <dani.eichhorn@squix.ch>
2018-03-21 16:20:12 +0100

    Fix type error

--- examples/WorldClockDemo/WorldClockDemo.ino
+++ examples/WorldClockDemo/WorldClockDemo.ino
@@ -92,7 +92,7 @@ void drawProgress(OLEDDisplay *display, int percentage, String label) {
   display->display();
 }
 
-void drawClock(OLEDDisplay *display, int x, int y, int timeZoneIndex, String city, const char* icon) {
+void drawClock(OLEDDisplay *display, int x, int y, int timeZoneIndex, String city, const uint8_t* icon) {
   display->setTextAlignment(TEXT_ALIGN_LEFT);
   display->setFont(ArialMT_Plain_10);
   display->drawString(x + 60, y + 5, city);
