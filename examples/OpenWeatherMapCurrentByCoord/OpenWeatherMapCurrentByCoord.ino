ef1d2913977e23e611f00923d57f75066e612855
MarcFinns <marcello@fini.org>
2018-10-02 00:14:35 +0200

    Examples by coord

---
+++ examples/OpenWeatherMapCurrentByCoord/OpenWeatherMapCurrentByCoord.ino
@@ -0,0 +1,156 @@
+/**The MIT License (MIT)
+
+  Copyright (c) 2018 by ThingPulse Ltd., https://thingpulse.com
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <JsonListener.h>
+#include <time.h>
+#include "OpenWeatherMapCurrent.h"
+
+
+// initiate the client
+OpenWeatherMapCurrent client;
+
+// See https://docs.thingpulse.com/how-tos/openweathermap-key/
+String OPEN_WEATHER_MAP_APP_ID = "XXX";
+
+
+float LATITUDE = 42.045;
+float LONGITUDE = 6.37;
+
+/*
+  Arabic - ar, Bulgarian - bg, Catalan - ca, Czech - cz, German - de, Greek - el,
+  English - en, Persian (Farsi) - fa, Finnish - fi, French - fr, Galician - gl,
+  Croatian - hr, Hungarian - hu, Italian - it, Japanese - ja, Korean - kr,
+  Latvian - la, Lithuanian - lt, Macedonian - mk, Dutch - nl, Polish - pl,
+  Portuguese - pt, Romanian - ro, Russian - ru, Swedish - se, Slovak - sk,
+  Slovenian - sl, Spanish - es, Turkish - tr, Ukrainian - ua, Vietnamese - vi,
+  Chinese Simplified - zh_cn, Chinese Traditional - zh_tw.
+*/
+String OPEN_WEATHER_MAP_LANGUAGE = "en";
+boolean IS_METRIC = true;
+
+/**
+   WiFi Settings
+*/
+const char* ESP_HOST_NAME = "esp-" + ESP.getFlashChipId();
+const char* WIFI_SSID     = "your wifi";
+const char* WIFI_PASSWORD = "your password!";
+
+// initiate the WifiClient
+WiFiClient wifiClient;
+
+
+
+/**
+   Helping funtions
+*/
+void connectWifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+
+/**
+   SETUP
+*/
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  connectWifi();
+
+  Serial.println();
+  Serial.println("\n\nNext Loop-Step: " + String(millis()) + ":");
+
+  OpenWeatherMapCurrentData data;
+  client.setLanguage(OPEN_WEATHER_MAP_LANGUAGE);
+  client.setMetric(IS_METRIC);
+  client.updateCurrentByCoord(&data, OPEN_WEATHER_MAP_APP_ID, LATITUDE, LONGITUDE);
+
+  Serial.println("------------------------------------");
+
+  // "lon": 8.54, float lon;
+  Serial.printf("lon: %f\n", data.lon);
+  // "lat": 47.37 float lat;
+  Serial.printf("lat: %f\n", data.lat);
+  // "id": 521, weatherId weatherId;
+  Serial.printf("weatherId: %d\n", data.weatherId);
+  // "main": "Rain", String main;
+  Serial.printf("main: %s\n", data.main.c_str());
+  // "description": "shower rain", String description;
+  Serial.printf("description: %s\n", data.description.c_str());
+  // "icon": "09d" String icon; String iconMeteoCon;
+  Serial.printf("icon: %s\n", data.icon.c_str());
+  Serial.printf("iconMeteoCon: %s\n", data.iconMeteoCon.c_str());
+  // "temp": 290.56, float temp;
+  Serial.printf("temp: %f\n", data.temp);
+  // "pressure": 1013, uint16_t pressure;
+  Serial.printf("pressure: %d\n", data.pressure);
+  // "humidity": 87, uint8_t humidity;
+  Serial.printf("humidity: %d\n", data.humidity);
+  // "temp_min": 289.15, float tempMin;
+  Serial.printf("tempMin: %f\n", data.tempMin);
+  // "temp_max": 292.15 float tempMax;
+  Serial.printf("tempMax: %f\n", data.tempMax);
+  // "wind": {"speed": 1.5}, float windSpeed;
+  Serial.printf("windSpeed: %f\n", data.windSpeed);
+  // "wind": {"deg": 1.5}, float windDeg;
+  Serial.printf("windDeg: %f\n", data.windDeg);
+  // "clouds": {"all": 90}, uint8_t clouds;
+  Serial.printf("clouds: %d\n", data.clouds);
+  // "dt": 1527015000, uint64_t observationTime;
+  time_t time = data.observationTime;
+  Serial.printf("observationTime: %d, full date: %s", data.observationTime, ctime(&time));
+  // "country": "CH", String country;
+  Serial.printf("country: %s\n", data.country.c_str());
+  // "sunrise": 1526960448, uint32_t sunrise;
+  time = data.sunrise;
+  Serial.printf("sunrise: %d, full date: %s", data.sunrise, ctime(&time));
+  // "sunset": 1527015901 uint32_t sunset;
+  time = data.sunset;
+  Serial.printf("sunset: %d, full date: %s", data.sunset, ctime(&time));
+
+  // "name": "Zurich", String cityName;
+  Serial.printf("cityName: %s\n", data.cityName.c_str());
+  Serial.println();
+  Serial.println("---------------------------------------------------/\n");
+
+}
+
+
+/**
+   LOOP
+*/
+void loop() {
+
+}
