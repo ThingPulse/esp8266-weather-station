20441ecac1d390f06b9747d8ea30bfe19b98f428
Marcel Stör <marcelstoer@users.noreply.github.com>
2018-12-25 21:43:48 +0100

    Use fmod instead of % for floating point modulo
    Fixes #148

--- src/TimeClient.cpp
+++ src/TimeClient.cpp
@@ -121,5 +121,5 @@ long TimeClient::getCurrentEpoch() {
 }
 
 long TimeClient::getCurrentEpochWithUtcOffset() {
-  return round(getCurrentEpoch() + 3600 * myUtcOffset + 86400L) % 86400L;
+  return fmod(round(getCurrentEpoch() + 3600 * myUtcOffset + 86400L), 86400L);
 }
